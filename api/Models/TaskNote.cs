49fedd7d1cd25c407e56c9b43883cf33d138ae87
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2024-04-12 23:21:39 +0300

    Test commit api

--- api/Models/TaskNote.cs
+++ api/Models/TaskNote.cs
@@ -6,6 +6,7 @@
         {
             Id = Guid.NewGuid();
         }
+
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public int SerialNumber { get; set; }
