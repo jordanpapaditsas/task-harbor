e9e7d855c94b1d63e6f888476b78c0fa6aaa0fe4
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2024-04-21 20:26:37 +0300

    .

--- api/obj/Debug/net8.0/task-harbor-API.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/task-harbor-API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task-harbor-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c414e517db012ae159c7bc185088f51ffb2fb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93036246748aabdd311235c411a56b29dbac6f9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("task-harbor-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task-harbor-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
